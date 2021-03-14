48c067b3e2263d0ae2b31c9a4238c5db964ad2cc
J. Cohen <hostelmarketing@gmail.com>
2021-03-13 20:38:50 -0800

    Fix tests for new output

--- src/components/discourse-topic-link/test/discourse-topic-link.spec.tsx
+++ src/components/discourse-topic-link/test/discourse-topic-link.spec.tsx
@@ -1,26 +1,39 @@
 import { h } from "@stencil/core";
 import { newSpecPage } from "@stencil/core/testing";
 import { DiscourseTopicLink } from "../discourse-topic-link";
+import { IDiscourseTopic } from "../../../utils/discourse";
 
 describe("discourse-topic-link", () => {
+    const topic: IDiscourseTopic = {
+        url: "https://meta.discourse.org/t/install-plugins-in-discourse/19157",
+        title: "Install Plugins in Discourse",
+        categoryId: 12,
+        likeCount: 5,
+        postsCount: 7,
+    };
+
     it("renders a default discourse link", async () => {
         const page = await newSpecPage({
             components: [DiscourseTopicLink],
             template: () => (
-                <discourse-topic-link
-                    topic={{
-                        url:
-                            "https://meta.discourse.org/t/install-plugins-in-discourse/19157",
-                        title: "Install Plugins in Discourse",
-                    }}
-                ></discourse-topic-link>
+                <discourse-topic-link topic={topic}></discourse-topic-link>
             ),
         });
         expect(page.root).toEqualHtml(`
             <discourse-topic-link>
-                    <li class="discourse-topic-link">
-                        <a href="https://meta.discourse.org/t/install-plugins-in-discourse/19157" target="_blank" rel="noreferrer noopener">Install Plugins in Discourse</a>
-                    </li>
+                <li class="category-12 discourse-topic-link">
+                    <a
+                        data-category-id="12"
+                        data-likes="5"
+                        data-posts-count="7"
+                        href="https://meta.discourse.org/t/install-plugins-in-discourse/19157"
+                        title="7 posts and 5 likes"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        Install Plugins in Discourse
+                    </a>
+                </li>
             </discourse-topic-link>
         `);
     });
@@ -31,19 +44,25 @@ describe("discourse-topic-link", () => {
             template: () => (
                 <discourse-topic-link
                     newWindow={false}
-                    topic={{
-                        url:
-                            "https://meta.discourse.org/t/install-plugins-in-discourse/19157",
-                        title: "Install Plugins in Discourse",
-                    }}
+                    topic={topic}
                 ></discourse-topic-link>
             ),
         });
         expect(page.root).toEqualHtml(`
             <discourse-topic-link>
-                    <li class="discourse-topic-link">
-                        <a href="https://meta.discourse.org/t/install-plugins-in-discourse/19157" rel="" target="">Install Plugins in Discourse</a>
-                    </li>
+                <li class="category-12 discourse-topic-link">
+                    <a
+                        data-category-id="12"
+                        data-likes="5"
+                        data-posts-count="7"
+                        href="https://meta.discourse.org/t/install-plugins-in-discourse/19157"
+                        title="7 posts and 5 likes"
+                        target=""
+                        rel=""
+                    >
+                        Install Plugins in Discourse
+                    </a>
+                </li>
             </discourse-topic-link>
         `);
     });
@@ -54,19 +73,25 @@ describe("discourse-topic-link", () => {
             template: () => (
                 <discourse-topic-link
                     noreferrer={false}
-                    topic={{
-                        url:
-                            "https://meta.discourse.org/t/install-plugins-in-discourse/19157",
-                        title: "Install Plugins in Discourse",
-                    }}
+                    topic={topic}
                 ></discourse-topic-link>
             ),
         });
         expect(page.root).toEqualHtml(`
             <discourse-topic-link>
-                    <li class="discourse-topic-link">
-                        <a href="https://meta.discourse.org/t/install-plugins-in-discourse/19157" rel="noopener" target="_blank">Install Plugins in Discourse</a>
-                    </li>
+                <li class="category-12 discourse-topic-link">
+                    <a
+                        data-category-id="12"
+                        data-likes="5"
+                        data-posts-count="7"
+                        href="https://meta.discourse.org/t/install-plugins-in-discourse/19157"
+                        title="7 posts and 5 likes"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        Install Plugins in Discourse
+                    </a>
+                </li>
             </discourse-topic-link>
         `);
     });
@@ -79,19 +104,25 @@ describe("discourse-topic-link", () => {
                     newWindow={false}
                     noreferrer={true}
                     noopener={false}
-                    topic={{
-                        url:
-                            "https://meta.discourse.org/t/install-plugins-in-discourse/19157",
-                        title: "Install Plugins in Discourse",
-                    }}
+                    topic={topic}
                 ></discourse-topic-link>
             ),
         });
         expect(page.root).toEqualHtml(`
             <discourse-topic-link>
-                    <li class="discourse-topic-link">
-                        <a href="https://meta.discourse.org/t/install-plugins-in-discourse/19157" rel="noreferrer" target="">Install Plugins in Discourse</a>
-                    </li>
+                <li class="category-12 discourse-topic-link">
+                    <a
+                        data-category-id="12"
+                        data-likes="5"
+                        data-posts-count="7"
+                        href="https://meta.discourse.org/t/install-plugins-in-discourse/19157"
+                        title="7 posts and 5 likes"
+                        target=""
+                        rel="noreferrer"
+                    >
+                        Install Plugins in Discourse
+                    </a>
+                </li>
             </discourse-topic-link>
         `);
     });
